983fd664cab996d5d8f4c12773a36b4b0d657d64
AndyOoh <andy.oeee@gmail.com>
2024-10-08 01:34:03 +0700

    fix header, footer layout

--- apps/web/app/components/Footer.tsx
+++ apps/web/app/components/Footer.tsx
@@ -1,3 +1,4 @@
 export function Footer() {
-  return <footer className='flex w-100 h-8 bg-purple border-2 border-green'>Footer</footer>;
+  // return <footer className='bottom-0 flex w-100 h-8 bg-purple-200 border-2 border-green'>Footer</footer>;
+  return <header className='bottom-0 flex justify-between items-center p-4 bg-gray-800 text-white'>Footer here</header>;
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import { Flex, Text, Button, Theme, ThemePanel } from "@radix-ui/themes";
+import { Flex, Text, Button, Theme, ThemePanel, Container } from "@radix-ui/themes";
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "./config/wagmi.config";
 import { Header } from "./components/Header";
-import { Footer } from './components/Footer';
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
     <Theme accentColor='jade' grayColor='slate'>
       <WagmiProvider config={wagmiConfig}>
-        <Header />
-        <main className=''>
-          <h1>Welcome to Tiny Dapp</h1>
-          <Flex direction='column' gap='2'>
-            <Text>Hello from Radix Themes</Text>
-            <Button>Let's go</Button>
-          </Flex>
-        </main>
-        <footer className=''>Footer</footer>
-        <Footer />
+        <Flex px='0' direction='column' className='h-screen bg-purple-50/10'>
+          <Header />
+          <Container size='1'>
+            <main className=''>
+              <h1>Welcome to Tiny Dapp</h1>
+              <Flex direction='column' gap='2'>
+                <Text>Hello from Radix Themes</Text>
+                <Button>Let's go</Button>
+              </Flex>
+            </main>
+          </Container>
+          <Footer />
+        </Flex>
       </WagmiProvider>
-      <ThemePanel />
+      {/* <ThemePanel /> */}
     </Theme>
   );
 }
